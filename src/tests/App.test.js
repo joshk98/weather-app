6e776b2ca6ac5c8e0df5b5971d25c0dc856b6362
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-06-09 11:40:08 +0100

    Added testing capabilities for LocationDetails

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../components/App";
+import forecast from "../data/forecast.json";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/weather app/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App location={forecast.location} />);
+  const h1element = screen.getByText(/Manchester, UK/i);
+  expect(h1element).toBeInTheDocument();
 });
